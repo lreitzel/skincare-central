45c994f7055e7038a8f6a28a839a2c4df7bffbca
Lydia <lreitzel@gmail.com>
2021-06-23 15:07:38 -0400

    added login/logout routes and user validations

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :products
   resources :reviews
   resources :users
+
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+  post '/logout', to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
