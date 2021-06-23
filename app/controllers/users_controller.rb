bdd1c363bd1cebad63a226369f9896eaa2cc1392
Lydia <lreitzel@gmail.com>
2021-06-23 16:24:46 -0400

    added helper and updated user_controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :current_user, only: [:edit, :update]
+    before_action :current_user, only: [:edit, :update, :show]
 
     def index #needs view showing all users?
         @users = User.all
@@ -10,18 +10,38 @@ class UsersController < ApplicationController
     end
     
     def create
+        @user = User.create(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(current_user)
+        else
+            render :new, alert: "Sign-Up Unsuccessful, Please Try Again"
+        end
     end
 
     def show #profile page
+        if @user.exists?
+            render :show
+        else
+            redirect_to users_path, alert: "User not found"
+        end
     end
 
     def edit
     end
 
     def update
+        @user.update(user_params)
+        if @user.valid?
+            redirect_to user_path(@user)
+        else
+            render :edit
+        end
     end
 
     def destroy
+        session.clear
+        redirect_to users_path
     end
     
     private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+    def full_name
+        @user.first_name + @user.last_name
+    end
+
     def skin_types
         ["Normal", "Dry", "Oily", "Combination"]
     end
