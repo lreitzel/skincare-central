5ec38edc856ca17b335aac94b0b99b832d736652
Lydia <lreitzel@gmail.com>
2021-06-23 14:26:05 -0400

    added views files and controller actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,29 @@
 class ReviewsController < ApplicationController
+
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:rating, :review_body, :image)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+    def index #needs view showing all users?
+    end
+
+    def new #signup
+    end
+    
+    def create
+    end
+
+    def show #profile page
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
