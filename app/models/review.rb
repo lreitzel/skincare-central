a26255e2bccdb1bf59661592bc419825e3cf105d
Lydia <lreitzel@gmail.com>
2021-06-25 17:34:10 -0400

    added product attributes

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,11 @@ class Review < ApplicationRecord
     belongs_to :product
     belongs_to :user
     validates_presence_of :review_body, :rating
+    # accepts_nested_attributes_for :products
     
+    def product_attributes=(product)
+        self.product = Product.find_or_create_by(name: product[:name])
+        
+    end
+
 end
