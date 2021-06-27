90d4ac6f38b64b5337f1b833104a51853a13d6f0
Lydia <lreitzel@gmail.com>
2021-06-27 17:10:35 -0400

    added user_id and product_id migrations

---
+++ db/migrate/20210626215336_change_type_to_category_in_products.rb
@@ -0,0 +1,5 @@
+class ChangeTypeToCategoryInProducts < ActiveRecord::Migration[6.1]
+  def change
+    rename_column :products, :type, :category
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_22_212428) do
+ActiveRecord::Schema.define(version: 2021_06_26_215336) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "category"
     t.string "brand"
     t.string "price"
     t.datetime "created_at", precision: 6, null: false
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2021_06_22_212428) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "product_id"
   end
 
   create_table "users", force: :cascade do |t|
