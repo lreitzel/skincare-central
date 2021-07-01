69cb33903eafb925feb5528b9169216fb0843d44
Lydia <lreitzel@gmail.com>
2021-07-01 00:17:49 -0400

    removed image column from reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_26_215336) do
+ActiveRecord::Schema.define(version: 2021_07_01_041555) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 2021_06_26_215336) do
   create_table "reviews", force: :cascade do |t|
     t.string "rating"
     t.string "review_body"
-    t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
