f2503ddeb4e4900137f17c992c63a455d5665b06
Lydia <lreitzel@gmail.com>
2021-07-01 01:06:12 -0400

    edited products index/form pages

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,9 @@ class Product < ApplicationRecord
     has_many :users, through: :reviews
     validates_presence_of :name, :brand
 
+    # scope: products_of_one_brand ->(skincare_brand) { where(brand: skincare_brand) }
+    # scope: most_reviewed_product ->()
+
+    PRICE = ["$", "$$", "$$$"]
+
 end
