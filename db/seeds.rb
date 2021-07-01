ac7d7402a7a03a1dad8b049e8ab10a55ef9501eb
Lydia <lreitzel@gmail.com>
2021-07-01 00:54:32 -0400

    removed nest attributes method and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# product_1 = Product.create(name: "Milky Jelly Cleanser", category: "Cleanser", brand: "Glossier", price: "$")
+# product_2 = Product.create(name: "Moisture Renewal Oil Booster", category: "Serum", brand: "Paula's Choice", price: "$$")
+# product_3 = Product.create(name: "SOS Daily Rescue Facial Spray", category: "Spray", brand: "Tower 28 Beauty", price: "$$")
+
+# review_1 = Review.create(rating: "5", review_body: "The best", user_id: 1, product_id: 1)
+# review_2 = Review.create(rating: "5", review_body: "I love this serum", user_id: 1, product_id: 2)
+# review_3 = Review.create(rating: "5", review_body: "This helped my rosacea so much", user_id: 2, product_id: 3)
+
+
+
